8755f64d5134695cd89c72a2e43d9360bf4c95d7
daehyun <detail54@nate.com>
2022-07-11 18:06:39 +0900

    path id값 getServerSideProps로 전달

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,33 +1,29 @@
 import { NextPage } from 'next'
-import { GetStaticProps } from 'next'
 import { useRouter } from 'next/router'
 import { IPost } from 'hooks/api/interface'
 import API_URL from 'hooks/api/urls'
 import usePost from 'hooks/usePost'
 import { usePrefetchQuery } from 'hooks/useReactQuery'
+import { GetServerSideProps } from 'next'
 
 interface IProps {
   id: number
 }
 
-const post: NextPage = () => {
-  const router = useRouter()
-  const id = Number(router.query.id)
+const post: NextPage<IProps> = ({ id }) => {
   const { getPost } = usePost()
-  const { data: postData, isLoading, error, isError } = getPost(id)
+  const { data: postData } = getPost(id)
 
   return <div>{postData?.body}</div>
 }
 
 export default post
 
-// export const getStaticProps: GetStaticProps<IProps> = async (ctx) => {
-//   const router = useRouter()
-//   const id = Number(router.query.id)
-//   console.log(id)
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const id = ctx.query.id
 
-//   usePrefetchQuery<IPost>(API_URL.POSTS, { id })
-//   return {
-//     props: { id },
-//   }
-// }
+  usePrefetchQuery<IPost>(API_URL.POSTS, { id })
+  return {
+    props: { id },
+  }
+}
