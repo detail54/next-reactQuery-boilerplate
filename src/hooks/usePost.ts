7913a08098d16f7f41e027f06436e93e271f5dce
daehyun <detail54@nate.com>
2022-07-11 17:16:44 +0900

    usePost 훅 추가

---
+++ src/hooks/usePost.ts
@@ -0,0 +1,46 @@
+import API_URL from './api/urls'
+// interface
+import { IPost } from './api/interface'
+// hooks
+import {
+  useQuery,
+  usePostMutation,
+  TMutationErr,
+  TQueryErr,
+} from './useReactQuery'
+
+const usePost = () => {
+  const getPosts = (onError?: TQueryErr) => {
+    return useQuery<IPost[]>(API_URL.POSTS, undefined, onError)
+  }
+
+  const getPost = (id: number, onError?: TQueryErr) => {
+    return useQuery<IPost>(
+      `${API_URL.POSTS}/${id}`,
+      undefined,
+      onError,
+      undefined,
+      { cacheTime: 100000 },
+    )
+  }
+
+  const addPost = (
+    updater: (oldData: IPost[], newData: IPost) => IPost[],
+    onError?: TMutationErr,
+  ) => {
+    return usePostMutation<IPost[], IPost>(
+      API_URL.POSTS,
+      undefined,
+      updater,
+      onError,
+    )
+  }
+
+  return {
+    getPosts,
+    getPost,
+    addPost,
+  }
+}
+
+export default usePost
