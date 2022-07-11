87106b0b66cb47cae3199b0cd71a8c2d2d68f268
daehyun <detail54@nate.com>
2022-07-11 17:16:58 +0900

    env 설정 추가

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_SERVER_URL,
+  baseURL: process.env.NEXT_PUBLIC_APP_API_SERVER_URL,
   headers: {
     'Content-Type': 'application/json; charset=UTF-8',
     'X-Access-Token': 'access-token',
