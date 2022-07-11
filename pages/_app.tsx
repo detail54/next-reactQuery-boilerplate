f01ff8099634b1bcbbded5c789127b05d29a442a
daehyun <detail54@nate.com>
2022-07-11 15:25:02 +0900

    ssr 기본설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Hydrate, QueryClientProvider } from 'react-query'
+import queryClient from 'utils/reactQuery'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 }
 
-export default MyApp
+export default App
