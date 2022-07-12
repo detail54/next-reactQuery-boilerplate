53ccec72f06d2d24eb8966ab849d4616ba34701b
daehyun <detail54@nate.com>
2022-07-12 16:39:10 +0900

    styled component 세팅

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,42 @@
-import '../../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Hydrate, QueryClientProvider } from 'react-query'
 import queryClient from 'utils/reactQuery'
 import { ReactQueryDevtools } from 'react-query/devtools'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from 'styles/GlobalStyle'
+import { useState } from 'react'
+import RightTheme from 'styles/ThemeRight'
+import DarkTheme from 'styles/ThemeDark'
+import Header from 'layout/Header'
+import Footer from 'layout/Footer'
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false)
+
+  const changeThemeButtonText = isDarkMode ? 'right mode' : 'dark mode'
+
+  const onChangeTheme = () => {
+    setIsDarkMode(!isDarkMode)
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </Hydrate>
+      <ThemeProvider theme={isDarkMode ? DarkTheme : RightTheme}>
+        <GlobalStyle />
+        <Hydrate state={pageProps.dehydratedState}>
+          <Header
+            changeThemeButtonText={changeThemeButtonText}
+            onChangeTheme={onChangeTheme}
+          />
+          <Component {...pageProps} />
+          <Footer />
+          <ReactQueryDevtools
+            initialIsOpen={false}
+            position='bottom-right'
+            panelProps={{ className: 'devtools' }}
+          />
+        </Hydrate>
+      </ThemeProvider>
     </QueryClientProvider>
   )
 }
---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  body {
+    font-size: 1rem;
+    margin: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: ${({ theme }) => theme.backgroundColor};
+    color: ${({ theme }) => theme.fontColors.themeColor};
+    
+    #__next {
+      display: block;
+    }
+  }
+  section, article, header, main, footer, nav, span, div, ul, ol, li, a, h1, h2, h3, h4, h5, h6, button, p {
+    display: flex;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 500;
+  }
+  ul, ol {
+    list-style: none;
+  }
+  button, div, span, li {
+    align-items: center;
+    justify-content: center;
+  }
+  a {
+    color: black;
+    text-decoration: none;
+  }
+  button {
+    cursor: pointer;
+  }
+  // react-query devtools style
+  .devtools {
+    align-items: flex-start;
+    & div, & span {
+      display: block;
+      align-items: initial;
+      justify-content: initial;
+    }
+    & > div + div > div + div + div + div {
+      display: flex;
+      button {
+        margin-right: 5px;
+      }
+      span {
+        margin-left: 5px;
+        margin-right: 5px;
+      }
+    }
+  }
+`
