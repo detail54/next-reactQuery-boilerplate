dc4e6062d9f506ca7e67a0c96a7cedbe31d31289
daehyun <detail54@nate.com>
2022-07-18 14:14:41 +0900

    eslint 맞춰서 수정

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,10 +1,8 @@
-import { NextPage } from 'next'
-import { useRouter } from 'next/router'
+import { NextPage, GetServerSideProps } from 'next'
 import { IPost } from 'hooks/api/interface'
 import API_URL from 'hooks/api/urls'
 import usePost from 'hooks/usePost'
 import { usePrefetchQuery } from 'hooks/useReactQuery'
-import { GetServerSideProps } from 'next'
 
 interface IProps {
   id: number
@@ -20,7 +18,7 @@ const post: NextPage<IProps> = ({ id }) => {
 export default post
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const id = ctx.query.id
+  const { id } = ctx.query
 
   await usePrefetchQuery<IPost>(API_URL.POSTS, { id })
   return {
--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -4,10 +4,9 @@ import usePost from 'hooks/usePost'
 import { usePrefetchQuery } from 'hooks/useReactQuery'
 import { GetStaticProps, NextPage } from 'next'
 import Link from 'next/link'
-import PostsMain from './index.styles'
 import Board from 'components/templates/board/Board'
 import { IListItemProps } from 'components/molecules/listItem/ListItem'
-import { dehydrate } from 'react-query'
+import PostsMain from './index.styles'
 
 const posts: NextPage = () => {
   const { getPosts } = usePost()
@@ -37,6 +36,7 @@ const posts: NextPage = () => {
 
 export default posts
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
-  return await usePrefetchQuery<IPost[]>(API_URL.POSTS)
+export const getStaticProps: GetStaticProps = async () => {
+  const props = await usePrefetchQuery<IPost[]>(API_URL.POSTS)
+  return props
 }
